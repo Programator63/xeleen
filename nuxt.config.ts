b1e50abbc700bc1c22a9eddcf942e1cf364fc625
Programator63 <programator63@gmail.com>
2024-10-11 02:06:36 +0300

    db change to postgres 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,27 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
+  // https://nuxt.com/docs/api/configuration/nuxt-config
+  export default defineNuxtConfig({
+    compatibilityDate: '2024-04-03',
+    devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/icon',
-    '@nuxt/ui',
-    'nuxt-auth-utils',
-    '@prisma/nuxt'
-  ],
-  prisma: {
-    autoSetupPrisma: true,
-  },
-  colorMode: {
-    preference: 'no-preference',
-  },
-  build: {
-    rollupOptions: {
-      external: ['@prisma/nuxt'],
+    modules: [
+      '@nuxt/icon',
+      '@nuxt/ui',
+      'nuxt-auth-utils',
+      '@prisma/nuxt'
+    ],
+    prisma: {
+      autoSetupPrisma: true,
+      migrate: {
+        enabled: true,
+        mode: 'deploy', // Set the mode to deploy for non-interactive environment
+      },
     },
-  },
-})
+    colorMode: {
+      preference: 'no-preference',
+    },
+    build: {
+      rollupOptions: {
+        external: ['@prisma/nuxt'],
+      },
+    },
+  })
